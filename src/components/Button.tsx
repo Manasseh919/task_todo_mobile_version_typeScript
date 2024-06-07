d92208c022bb117b3799fc26f11b53a0346a9b76
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-06-07 20:01:19 +0200

    continue from windows

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <View>
+      <Text>Button this is working erfectly</Text>
+    </View>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({})
